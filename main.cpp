f8890391d650774a6badc93ad5fbc41fa7f00a4c
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2019-06-20 18:07:13 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -38,11 +38,7 @@ void pause_n_clear()
 #ifdef _WIN32
     system("PAUSE");
     system("CLS");
-#elif __gnu_linux
-    system("echo Press any key to continue");
-    system("read -p ... .");
-    system("clear");
-#elif __APPLE__
+#elif __unix__
     system("echo Press any key to continue");
     system("read -p ... .");
     system("clear");
