2974db5a8fe06e87b1b2768e7515f8b9e404c9d6
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2019-11-28 19:19:53 +0100

    Update Admiral.h

--- Admiral.h
+++ Admiral.h
@@ -4,12 +4,14 @@
 #define ADMIRAL_H
 struct Admiral
 {
-    int fleet_size=0;
-    char attack_board[10][10];
-    char defend_board[10][10];
+    int fleet_size;
+    int is_player;
+
+    Admiral(int = 0,int = 1);
     void set_battleships(int c,char defend_board[10][10]);
     void shot_battleships(char attack_board[10][10],char defend_board[10][10]);
     void display_board(char board[10][10]);
     void create_empty_board(char board[10][10]);
+    ~Admiral();
 };
 #endif // ADMIRAL_H
