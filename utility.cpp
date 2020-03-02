deb3c54f38b9585f898c6ad5b14a022fe4df9036
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2020-03-02 11:06:16 +0100

    Update utility.cpp

--- utility.cpp
+++ utility.cpp
@@ -2,17 +2,15 @@
 
 void pause_n_clear()
 {
+cout << "Press any key to continue" << endl;
+char ch;
+cin.get.get(ch);
 #ifdef _WIN32
-    system("PAUSE");
     system("CLS");
 #elif __unix__
-    system("echo Press any key to continue");
-    system("read -p ... .");
     system("clear");
 #elif __APPLE__
     cout << "APPLE..." << endl;
-    system("echo Press any key to continue");
-    system("read -p ... .");
     system("clear");
 #else
     cout << "OS not supported!" << endl;
